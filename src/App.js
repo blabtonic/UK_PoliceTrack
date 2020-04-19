a52575f737d28f9ff98a31ed9cdf69c3f9542c41
Trystan Hendricks <dolkhound@gmx.com>
2020-04-18 23:59:40 -0400

    removed control component zooming working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 //import ListApi from './components/ListApi';
 import MonthPicker from './components/MonthPicker';
 import Map from 'pigeon-maps';
-import Marker from 'pigeon-marker';
-import Button from 'react-bootstrap/Button';
+import Marker from 'pigeon-marker'
+import Mapper from './components/MapComponent';
 
 
 function App() {
   return (
     <div className="App">
       <MonthPicker></MonthPicker>
-      <Map center={[51.507, -0.0612]} zoom={11} width={900} height={900} zoomOnMouseWheel={true}>
-        <Marker anchor={[51.507, -0.0612]} payload={1}></Marker>
-      </Map>
-      <Button variant="primary">Zoom Out</Button>
+      <Mapper></Mapper>
     </div>
   );
 }
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import Map from 'pigeon-maps';
-import Marker from 'pigeon-marker'
+import Marker from 'pigeon-marker';
+import Button from 'react-bootstrap/Button';
 
 class Mapper extends Component {
     constructor (props){
         super(props)
 
         this.state = {
-            center: [51.507, -0.0612],
-            zoom:14,
+            center: [51.429, -2.825],
+            zoom:5,
             provider: 'osm',
             metaWheelZoom: true,
         }
 
+    }
+
+    zoomIn = () => {
+        this.setState({
+            zoom: Math.min(this.state.zoom + 1, 20)
+        });
+    }
+
+    zoomOut = () => {
+        this.setState({
+            zoom: Math.max(this.state.zoom - 1, 1)
+        });
     }
 
     handleMarkerClick = ({event, payload, anchor}) => {
@@ -20,10 +33,16 @@ class Mapper extends Component {
     }
 
     render() {
+        //used to set the actual state
+        const { center, zoom } = this.state;
         return(
-            <Map center={this.center} zoom={this.zoom} width={900} height={900} metaWheelZoom={this.metaWheelZoom}>
-                
-            </Map>
+            <div className="MapBoard">
+                <Map center={center} zoom={zoom} width={900} height={900} zoomOnMouseWheel={true}>
+                    <Marker anchor={[51.507, -0.0612]} payload={1} onClick={({ event, anchor, payload }) => {}}></Marker>
+                </Map>
+                <Button variant="primary" onClick={this.zoomIn}>+</Button>
+                <Button variant="primary"onClick={this.zoomOut}>-</Button>
+            </div>
         );
     }
 }
