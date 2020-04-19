49d799003bc1611d388666da73035ecd0eb68412
Trystan Hendricks <dolkhound@gmx.com>
2020-04-18 23:03:02 -0400

    zoomOut button added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 //import ListApi from './components/ListApi';
 import MonthPicker from './components/MonthPicker';
 import Map from 'pigeon-maps';
-import Marker from 'pigeon-marker'
-//import Overlay from 'pigeon-maps';
+import Marker from 'pigeon-marker';
+import Button from 'react-bootstrap/Button';
 
 
 function App() {
@@ -11,8 +11,9 @@ function App() {
     <div className="App">
       <MonthPicker></MonthPicker>
       <Map center={[51.507, -0.0612]} zoom={11} width={900} height={900} zoomOnMouseWheel={true}>
-        <Marker anchor={[51.507, -0.0612]} payload={1} onClick={({ event, anchor, payload }) => {}}></Marker>
+        <Marker anchor={[51.507, -0.0612]} payload={1}></Marker>
       </Map>
+      <Button variant="primary">Zoom Out</Button>
     </div>
   );
 }
