5fd45a5d6bbb88a7e6dd3e070184d76e7d15cdf2
Trystan Hendricks <dolkhound@gmx.com>
2020-05-08 21:40:48 -0400

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Mapper from './components/MapComponent';
 import Form from './components/FormSubmit';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <Mapper></Mapper>
-      <Form></Form>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">DataTrack</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#violent" eventKey="disabled" disabled>Violent Crimes</Nav.Link>
+        <Nav.Link href="#search">Search and stop</Nav.Link>
+      </Nav>
+    </Navbar>
+    <br/>
+    <Mapper></Mapper>
+    <br/>
+    <Form></Form>
     </div>
   );
 }
