5ec5018d23c7e37b2fe38932e26eab09af70afa3
Trystan Hendricks <dolkhound@gmx.com>
2020-05-16 19:09:06 -0400

    working on routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class Header extends Component {
 	render () {
   	return (
-			<Navbar bg="dark" variant="dark">
-				<Navbar.Brand href="#home">DataTrack</Navbar.Brand>
-				<Nav className="mr-auto">
-					<Nav.Link href="#home">Home</Nav.Link>
-					<Nav.Link href="#violent" eventKey="disabled" disabled>Violent Crimes</Nav.Link>
-					<Nav.Link href="#search">Search and stop</Nav.Link>
-				</Nav>
-			</Navbar>
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand>DataTrack</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/violent-crime" eventKey="disabled" disabled>Violent Crimes</Nav.Link>
+              <Nav.Link href="/search">Search and stop</Nav.Link>
+            </Nav>
+        </Navbar>
+        <Switch>
+          <Route path="/search">
+            <Search/>
+          </Route>
+        </Switch>
+      </Router>
     );
     }
+}
+
+function Search() {
+  return <h2>Search n stop</h2>
 }
 
 
