c59717a8c76221efd51c16b9a2558bb9220d1fc1
Trystan Hendricks <dolkhound@gmx.com>
2021-05-30 18:09:31 -0400

    fixed routing about component is showing without map

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import Mapper from './components/MapComponent';
 import Form from './components/FormSubmit';
 import Header from './components/Header';
 import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Payment from './components/PayButton';
+import About from './components/About';
+import Help from './components/Help';
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Header />
-        <Row>
-          <Col sm>
-            <Mapper />
-          </Col>
-          <Col sm>
-            <Form />
-          </Col>
-        </Row>
-        <br />
-        <p>Only Bitcoin Cash(BCH) not Bitcoin(BTC)</p>
-        <Payment />
-      </Container>
-    </div>
+    <Router>
+      <div className="App">
+        <Container>
+          <Header />
+          <Switch>
+            <Route path="/about" component={About}></Route>
+            <Route path="/help" component={Help}></Route>
+            {/* Within the home switch display the map and form components */}
+            <Route path="/">
+              <Row>
+                <Col sm>
+                  <Mapper />
+                </Col>
+                <Col sm>
+                  <Form />
+                </Col>
+              </Row>
+              <br />
+              <Payment />
+            </Route>
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import About from './About';
-import Help from './Help';
 
 class Header extends Component {
   render() {
     return (
-      <Router>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand>DataTrack</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Link to="/">Home</Link>
-            <Nav.Link href="/violent-crime" eventKey="disabled" disabled>
-              Violent Crimes
-            </Nav.Link>
-            <Nav.Link href="/search">Search and stop</Nav.Link>
-            <Nav.Link href="/help">Help</Nav.Link>
-          </Nav>
-        </Navbar>
-        <Route path="/help" component={Help} />
-        <Route path="/About" component={About} />
-      </Router>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>DataTrack</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/violent-crime" eventKey="disabled" disabled>
+            Violent Crimes
+          </Nav.Link>
+          <Nav.Link href="/search">Search and stop</Nav.Link>
+          <Nav.Link href="/help">Help</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+      </Navbar>
     );
   }
 }
